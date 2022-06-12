27ac96ff5f1de48fd4ac58d51470b9b576b552b0
Jakub-Klejczyk <80176594+Jakub-Klejczyk@users.noreply.github.com>
2022-06-12 17:56:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,32 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const btnAdd = document.querySelector(".plus");
+const btnMin = document.querySelector(".minus");
+let iteamsNumber = document.querySelector(".number-input");
+let number = 0;
+
+btnAdd.addEventListener("click", function (e) {
+  number++;
+  iteamsNumber.value = number;
+});
+
+btnMin.addEventListener("click", function (e) {
+  if (number > 0) {
+    number--;
+    iteamsNumber.value = number;
+  }
+});
+
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+const sideBar = document.querySelector(".side-bar");
+menu.addEventListener("click", function (e) {
+  sideBar.style.display = "block";
+});
+
+const closeMenu = document.querySelector(".close");
+closeMenu.addEventListener("click", function (e) {
+  sideBar.style.display = "none";
+});
